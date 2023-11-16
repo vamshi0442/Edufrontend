9d0ea451c12b89fe062350f3bc64cbba6424c29a
Vamshi <Vamshisoftwaredeveloper@gmail.com>
2023-11-16 15:44:37 +0000

    added

---
+++ PatashalaUI/src/environments/environment.production.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production:true,
+    development:false,
+    apiurl:"https://patashalapi.azurewebsites.net/",
+    clienturl:"https://patashalaui.azurewebsites.net/"
+
+};
