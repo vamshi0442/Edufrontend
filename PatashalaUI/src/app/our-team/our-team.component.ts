23340bf9ea5f881c7920f798685769685fdeece0
Vamshi <Vamshisoftwaredeveloper@gmail.com>
2023-11-18 18:29:53 +0000

    added

--- PatashalaUI/src/app/our-team/our-team.component.ts
+++ PatashalaUI/src/app/our-team/our-team.component.ts
@@ -20,37 +20,30 @@ export class OurTeamComponent {
   ) {
   }
   ngOnInit(){
-    
     this.route.queryParams.subscribe(params=>{
       
       this.menuId= params['menuId'];
     });
 
-    this.ourteamresponses = [];
-    // this.httpClient.get<any>("assets/menudata.json").subscribe((data)=>{
+    // this.httpClient.get<any>("assets/data.json").subscribe((data)=>{
       this.apiService.getData().subscribe((data:any)=>{
-        
+      
       this.listMenuResponse = data.listMenuSubMenu;
-      let response: any = [];
-          this.listMenuResponse.forEach((element: { listMenuResponse: any; menu_Id:any }) => {
-            
-            response = element.listMenuResponse.filter((p: { menu_Id: any; }) => 
-             // p.menu_Id == this.menuId);
-            p.menu_Id == 7);
-
-            if(response.length != 0){
-               this.ourteamresponses  = response;
-               console.log(JSON.stringify(this.ourteamresponses ))
-            }
-            else{
-              // console.log(JSON.stringify(this.menuId + response))
-            }
-           
-
+          this.listMenuResponse.forEach((element: { listMenuResponse: any; }) => {
+            element.listMenuResponse.forEach((x: { responses: any; menu_Id :any;responses_Subheading:any })=>
+              {
+                // menu_Id:9 submenu_id:2
+               // if(x.menu_Id == this.menuId){
+                if(x.menu_Id == 7){                  
+                  this.ourteamresponses.push({menu:x.responses,menus:x.menu_Id});
+                }
+                
+              })
           });
-        });
-       
     }
     
+    );
+   
+   
   }
-
+}
